0ab62bbdf66ee1117ce7df3f375fb45083912260
marks214 <aimee.oz.x@gmail.com>
2020-11-03 10:07:28 -0800

    added route and controller for drivers new action

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -7,6 +7,9 @@ class DriversController < ApplicationController
     @driver = Driver.find_by(id: params[:id])
   end
 
+  def new
+    
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'trips#index'
+
+  get '/drivers/new', to: 'drivers#new'
   get '/trips', to: 'trips#index', as: 'trips'
   get '/drivers', to: 'drivers#index', as: 'drivers'
   get '/drivers/:id', to: 'drivers#show', as: 'driver'
