20da6af717e8d8db0b12aae988a68f528cac3c95
marks214 <aimee.oz.x@gmail.com>
2020-11-03 10:37:44 -0800

    added form and edit views, updated routes for all driver related actions

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -8,12 +8,23 @@ class DriversController < ApplicationController
   end
 
   def new
-    
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.new(driver_params)
+    result = @driver.save
+
+    if result
+      # redirect_to some path (e.g., driver_path(@path.id))
+    else
+      render :new
+    end
   end
 
   private
 
   def driver_params
-    # return params.require(:trip).permit(fields)
+    return params.require(:driver).permit(:id, :ride_id, :name, :vin, :available, :created_at, :updated_at)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'trips#index'
 
-  get '/drivers/new', to: 'drivers#new'
+  # TODO: change root route to homepages after merge
+  root to: 'trips#index'
   get '/trips', to: 'trips#index', as: 'trips'
+
+
+  # driver routes
   get '/drivers', to: 'drivers#index', as: 'drivers'
+  get '/drivers/new', to: 'drivers#new'
+  post '/drivers', to: 'drivers#create'
+
+
   get '/drivers/:id', to: 'drivers#show', as: 'driver'
+  get '/drivers/:id/edit', to: 'drivers#edit', as: 'edit_driver'
+  patch '/drivers/:id', to: 'drivers#update'
+  get 'drivers/:id/confirm_delete', to: 'drivers#confirm', as: 'confirm_driver'
+  delete '/drivers/:id', to: 'drivers#delete', as: 'delete_driver'
+  patch '/drivers/:id/complete', to 'drivers#complete', as: 'complete_driver'
+
 
 end
