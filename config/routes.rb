7418a03856a061b58e7f25bfa4af22e70ea48a8a
Emily Nagai <emnagai89@gmail.com>
2020-11-04 12:18:51 -0800

    dried up routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,14 @@ Rails.application.routes.draw do
   get '/trips', to: 'trips#index', as: 'trips'
 
   # Passengers routes
-  get '/passengers', to: 'passengers#index', as: 'passengers'
-  get '/passengers/new', to: 'passengers#new', as: 'new_passenger'
-  post '/passengers', to: 'passengers#create'
-
-  get '/passengers/:id', to: 'passengers#show', as: 'passenger'
-  get '/passengers/:id/edit', to: 'passengers#edit', as: 'edit_passenger'
-  patch '/passengers/:id', to: 'passengers#update'
-  delete '/passengers/:id', to: 'passengers#destroy'
+  resources :passengers
+  # get '/passengers', to: 'passengers#index', as: 'passengers'
+  # get '/passengers/new', to: 'passengers#new', as: 'new_passenger'
+  # post '/passengers', to: 'passengers#create'
+  #
+  # get '/passengers/:id', to: 'passengers#show', as: 'passenger'
+  # get '/passengers/:id/edit', to: 'passengers#edit', as: 'edit_passenger'
+  # patch '/passengers/:id', to: 'passengers#update'
+  # delete '/passengers/:id', to: 'passengers#destroy'
 
 end
