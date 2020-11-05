33d80a9f0542f6cccd9fb4482d25115d88de6b16
Emily Nagai <emnagai89@gmail.com>
2020-11-04 16:46:56 -0800

    conflicts resolved

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -21,23 +21,12 @@ describe TripsController do
     Trip.create ride_id: 1, driver_id: 1, passenger_id: 1, date: "2016-04-05", rating: 4, cost: 12.93
   }
   describe "show" do
-<<<<<<< HEAD
-    # it "can get a valid trip" do
-    #   get #nested route?
-    #
-    #   must_respond_with :success
-    # end
-    #trip
-    get trip_path(trip.id)
-    must_respond_with :success
-=======
     it "can get a valid trip" do
       trip
       get trip_path(trip.ride_id)
 
       must_respond_with :redirect
     end
->>>>>>> 645b9a9f673287ec23ae3abab745ca1a8b241119
   end
 
   describe "create" do
