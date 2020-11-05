555c36d9d7d15491b06c1ea17507b8b071355676
marks214 <aimee.oz.x@gmail.com>
2020-11-05 14:55:34 -0800

    updated trips index view and added buisness logic to driver model

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,4 +2,22 @@ class Driver < ApplicationRecord
   has_many :trips
   validates :name, presence: true
   validates :vin, length: { is: 17, message: "can't be blank"}, presence: true
+
+  def gross_earnings
+    gross_total = trips.sum(&:cost) / 100
+    return gross_total
+  end
+
+  def net_earnings
+    net_total = 0.8 * (gross_earnings - 1.65)
+    return net_total
+  end
+
+  def average_rating
+    sum_ratings = trips.sum(&:rating)
+    total_trips = trips.length
+    average = sum_ratings / total_trips
+    return average
+  end
+
 end
