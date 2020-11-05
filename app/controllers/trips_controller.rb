4bba2e3eafec63c553bf0e9b196a74d88d06ea8a
marks214 <aimee.oz.x@gmail.com>
2020-11-04 20:57:26 -0800

    updated driver show view to include accurate passenger info, gross earnings, and average rating

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -59,6 +59,6 @@ end
   private
 
   def trip_params
-    return params.require(:trip).permit(:id, :ride_id, :driver_id, :passenger_id, :date, :rating, :cost)
+    return params.require(:trip).permit(:id, :driver_id, :passenger_id, :date, :rating, :cost)
   end
   end
