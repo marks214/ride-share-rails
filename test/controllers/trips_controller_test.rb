60f3dd90e4ddab2c933cd8c6571ad02e9869b7d9
Emily Nagai <emnagai89@gmail.com>
2020-11-05 18:44:34 -0800

    can create a new trip for passenger and assign an available driver

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -1,29 +1,36 @@
 require "test_helper"
 
 describe TripsController do
-  before do
-    Trip.create(id: 1, driver_id: 2, passenger_id: 3, date: "2016-04-05", rating: 4, cost: 12.93)
-  end
-
-  let (:trip_hash) {
-    {
-        trip: {
-            id: 1,
-            driver_id: 2,
-            passenger_id: 3,
-            date: "2016-04-05",
-            rating: 4,
-            cost: 12.93
-        },
-    }
-  }
-  let (:trip) {
-    Trip.create id: 1, driver_id: 1, passenger_id: 1, date: "2016-04-05", rating: 4, cost: 12.93
-  }
+  # before do
+  #   Trip.create(id: 1, driver_id: 2, passenger_id: 3, date: "2016-04-05", rating: 4, cost: 12.93)
+  # end
+  #
+  # let (:trip_hash) {
+  #   {
+  #       trip: {
+  #           id: 1,
+  #           driver_id: 2,
+  #           passenger_id: 3,
+  #           date: "2016-04-05",
+  #           rating: 4,
+  #           cost: 12.93
+  #       },
+  #   }
+  # }
+  # let (:trip)
+  #   Trip.create id: 1, driver_id: 1, passenger_id: 1, date: "2016-04-05", rating: 4, cost: 12.93
+  # }
   describe "show" do
-    it "can get a valid trip" do
-      trip
+    it "can get a valid trip path" do
+      trip = Trip.create(id: 1, driver_id: 2, passenger_id: 3, date: "2016-04-05", rating: 4, cost: 12.93)
+
       get trip_path(trip.id)
+
+      must_respond_with :success
+    end
+
+    it "can redirect if there's in invalid trip" do
+      get trip_path(-1)
 
       must_respond_with :redirect
     end
