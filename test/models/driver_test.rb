37e21be000c44debb9d3e2fd4dfbcf64bc588a4a
marks214 <aimee.oz.x@gmail.com>
2020-11-06 13:11:39 -0800

    driver model test complete

--- test/models/driver_test.rb
+++ test/models/driver_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-describe "Driver" do
+describe Driver do
   let (:new_driver) {
     Driver.new(name: "Kari", vin: "12312312312312312", available: true)
   }
@@ -61,22 +61,37 @@ describe "Driver" do
 
   # Tests for methods you create should go here
   describe "custom methods" do
+    before(:all) do
+      @test_driver = Driver.create(name: "Safety First", vin: "12345678901234567", available: true)
+      @test_passenger = Passenger.create(name: "Sam Ple", phone_num: "555-555-5555")
+      @test_trip1 = Trip.create(driver_id: @test_driver.id, passenger_id: @test_passenger.id, date: "11/10/2020", rating: 1, cost: 1000)
+      @test_trip2 = Trip.create(driver_id: @test_driver.id, passenger_id: @test_passenger.id, date: "11/11/2020", rating: 5, cost: 5000)
+      @test_trip1.save
+      @test_trip2.save
+    end
+
+
     describe "average rating" do
-      # Your code here
+      it "will calculate the average rating for a driver" do
+        expect(@test_driver.average_rating).must_equal 3.0
+      end
     end
 
-    describe "total earnings" do
-      # Your code here
+    describe "gross earnings" do
+      it "will calculate the gross earnings for a driver (total cost of trips)" do
+        expect(@test_driver.gross_earnings).must_equal 60.0
+      end
+
     end
 
-    describe "can go online" do
-      # Your code here
+    describe "net earnings" do
+
+      it "will calculate the net earnings for a driver: 0.8 * (total cost - $1.65)" do
+        expect(@test_driver.net_earnings).must_be_close_to 46.68
+
+      end
     end
 
-    describe "can go offline" do
-      # Your code here
-    end
-
-    # You may have additional methods to test
   end
 end
+
